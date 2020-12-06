87b1d8d59cb087aab0f5a1eb9477ae76b43bd4b9
hosam mohamed <hosamelmalt@gmail.com>
2020-12-06 09:58:19 -0800

    Initial Commit

---
+++ Custom Carousel Slider/Custom_Carousel_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Carousel_SliderApp.swift
+//  Custom Carousel Slider
+//
+//  Created by Hossam on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Carousel_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
